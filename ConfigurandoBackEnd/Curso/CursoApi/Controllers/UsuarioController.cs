98ef58b7899992d9d1098eac572cc510b5f13339
Jojoreoli <66749358+Jojoreoli@users.noreply.github.com>
2021-03-09 23:17:38 -0300

    Projeto ao fim da primeira aula
    Curso Configuração da arquitetura back-end com .NET Core

---
+++ ConfigurandoBackEnd/Curso/CursoApi/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CursoApi.Models.Users;
+
+namespace CursoApi.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
+        {
+            return Created("", registroViewModelInput);
+        }
+    }
+}
---
+++ ConfigurandoBackEnd/Curso/CursoApi/Models/Users/RegistroViewModelInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoApi.Models.Users
+{
+    public class RegistroViewModelInput
+    {
+        [Required(ErrorMessage = "Login obrigatório")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Email obrigatório")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Senha obrigatória")]
+        public string Senha { get; set; }
+
+
+    }
+}
